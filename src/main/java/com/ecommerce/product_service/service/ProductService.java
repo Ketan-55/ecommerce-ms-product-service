a35b6ad80d78f51b1cc6348ce7322c788dc65e0f
Ketan Behere <Ketanbehere555@gmail.com>
2026-05-01 22:51:25 +0530

    feat: added logging for product creation flow

--- src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -4,6 +4,8 @@ import com.ecommerce.product_service.dto.ProductRequestDTO;
 import com.ecommerce.product_service.dto.ProductResponseDTO;
 import com.ecommerce.product_service.entity.Product;
 import com.ecommerce.product_service.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,15 @@ import java.time.LocalDateTime;
 
 @Service
 public class ProductService {
+
+    private static  final Logger log = LoggerFactory.getLogger(ProductService.class);
+
     @Autowired
     ProductRepository productRepository;
 
     public ProductResponseDTO createProduct(ProductRequestDTO request) {
         // Map ProductRequestDTO to Product entity
+        log.info("Creating product with name: {}", request.getName());
         Product product = new Product();
         product.setName(request.getName());
         product.setDescription(request.getDescription());
@@ -24,7 +30,12 @@ public class ProductService {
         // Set audit fields
         product.setCreatedAt(LocalDateTime.now());
         product.setCreatedBy("SYSTEM");
+
+        log.info("Saving product to MongoDB");
+        
         Product savedProduct = productRepository.save(product);
+
+        log.info("Product saved with id: {}", savedProduct.getId());
 
         ProductResponseDTO response = new ProductResponseDTO();
         response.setId(savedProduct.getId());
